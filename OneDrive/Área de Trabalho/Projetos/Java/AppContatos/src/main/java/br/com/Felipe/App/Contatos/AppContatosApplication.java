2c72992df851c7c60f08754bad7e91a0053659df
Felipe <felipeoliveira1993@live.com>
2024-03-06 08:59:56 -0300

    Iniciando projeto de contatos

---
+++ OneDrive/Área de Trabalho/Projetos/Java/AppContatos/src/main/java/br/com/Felipe/App/Contatos/AppContatosApplication.java
@@ -0,0 +1,13 @@
+package br.com.Felipe.App.Contatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppContatosApplication.class, args);
+	}
+
+}
---
+++ OneDrive/Área de Trabalho/Projetos/Java/AppContatos/src/test/java/br/com/Felipe/App/Contatos/AppContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Felipe.App.Contatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
